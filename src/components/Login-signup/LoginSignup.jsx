802c604750a6734f8a58cd59d05bdafe94ad72b7
ankitkr52 <ankit72p@gmail.com>
2026-02-01 00:08:23 +0530

    fix error

--- src/components/Login-signup/LoginSignup.jsx
+++ src/components/Login-signup/LoginSignup.jsx
@@ -34,7 +34,7 @@ const LoginSignup = () => {
       {action === "Sign Up" ? <div></div> : <div className="forgot-password">Lost password?<span>Click Here!</span></div>}
       <div className="submit-container">
         <div className={action === "Login" ? "submit gray" : "submit"} onClick={() => { setAction("Sign Up") }}>Sign Up</div>
-        <div className={action === "Sign Up" ? "submit gray" : "submit"} onClick={() => { setAction("Login") }}>Login</div>
+        <div className={action === "Sign Up" ? "submit gray" : "submit"} onClick={() => { setAction("Login) }}>Login</div>
       </div>
 
     </div>
